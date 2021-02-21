52c73af08dcce3959d2361315e654427d0cac702
David Librera <davidlibrera@gmail.com>
2021-02-21 03:02:57 +0100

    Fix source_url attribute

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule PhoenixBricks.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @elixir_requirements "~> 1.11"
 
   def project do
@@ -13,7 +13,7 @@ defmodule PhoenixBricks.MixProject do
       elixir: @elixir_requirements,
       package: package(),
       start_permanent: Mix.env() == :prod,
-      source_url: "https//github.com/davidlibrera/phoenix_bricks",
+      source_url: "https://github.com/davidlibrera/phoenix_bricks",
       version: @version
     ]
   end
