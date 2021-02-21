9728860d71d6b070c25c10f4b865b1d565131249
David Librera <davidlibrera@gmail.com>
2021-02-21 01:48:16 +0100

    Add LICENSE and metadata

--- mix.exs
+++ mix.exs
@@ -1,13 +1,20 @@
 defmodule PhoenixBricks.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @elixir_requirements "~> 1.11"
+
   def project do
     [
       app: :phoenix_bricks,
-      version: "0.1.0",
-      elixir: "~> 1.11",
+      deps: deps(),
+      description: @description,
+      docs: docs(),
+      elixir: @elixir_requirements,
+      package: package(),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      source_url: "https//github.com/davidlibrera/phoenix_bricks",
+      version: @version
     ]
   end
 
@@ -18,11 +25,33 @@ defmodule PhoenixBricks.MixProject do
     ]
   end
 
+  defp package do
+    [
+      maintainers: ["David Librera"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/davidlibrera/phoenix_bricks"},
+      files: ~w(lib LICENSE.md mix.exs README.md .formatter.exs)
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description do
+    """
+    Provide simple modules with common method for pure CRUD PhoenixLiveView
+    applications.
+    """
+  end
+
+  defp docs do
+    [
+      source_ref: "v#{@version}"
     ]
   end
 end
