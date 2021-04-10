59c5df6219eba30f0c6beeb8a9df1500824bc95d
David Librera <davidlibrera@gmail.com>
2021-04-10 02:34:56 +0200

    Validate schema name in generator

--- lib/mix/phx.bricks.gen.filter.ex
+++ lib/mix/phx.bricks.gen.filter.ex
@@ -1,6 +1,8 @@
 defmodule Mix.Tasks.Phx.Bricks.Gen.Filter do
   use Mix.Task
 
+  alias Mix.PhoenixBricks.Schema
+
   @shortdoc "Generates params filter logic for a resource"
 
   @moduledoc """
@@ -8,11 +10,35 @@ defmodule Mix.Tasks.Phx.Bricks.Gen.Filter do
   """
 
   @doc false
-  def run(_args) do
+  def run(args) do
     if Mix.Project.umbrella?() do
       Mix.raise(
         "mix phx.bricks.gen.filter must be invoked from within your *_web application root directory"
       )
     end
+
+    schema =
+      args
+      |> build()
+  end
+
+  defp build(args) do
+    {_opts, parsed} = OptionParser.parse!(args, strict: [])
+
+    [schema_name | filters] = validate_args!(parsed)
+
+    Schema.new(schema_name, filters)
+  end
+
+  defp validate_args!([]), do: raise_with_help("Schema name not provided")
+
+  defp validate_args!([schema | _filters] = args) do
+    if Schema.valid_schema_name?(schema), do: args, else: raise_with_help("Schema name not valid")
+  end
+
+  defp raise_with_help(msg) do
+    Mix.raise("""
+    #{msg}
+    """)
   end
 end
